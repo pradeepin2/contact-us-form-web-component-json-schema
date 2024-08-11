e05f8a8a3eaf328018d71f3167dc8b434cb3b470
Pradeep Kumar Saraswathi <pradeepin2@gmail.com>
2024-08-11 15:08:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,30 +6,25 @@ const fs = require('fs');
 
 // Array of Mime Types
 const mimeTypes = {
-  // Text Types
-  "html" : "text/html",
-  "css" : "text/css",
-  "js" : "text/javascript",
-  // Image Types
-  "jpeg" : "image/jpeg",
-  "jpg" : "image/jpeg",
-  "png" : "image/png",
-  "gif" : "image/gif",
-  "webp" : "image/webp",
-  "svg" : "image/svg+xml",
-  "icon" : "image/x-icon",
-  // Audio and Video Types
-  "webm" : "video/webm",
-  "ogg" : "video/ogg",
-  "mp4" : "video/mp4",
-  "mp3" : "audio/mpeg",
-  // Font Types
-  "ttf" : "font/ttf",
-  "otf" : "font/otf",
-  "woff" : "font/woff",
-  "woff2" : "font/woff2",
-  // Application Types
-  "pdf" : "application/pdf"
+    "html": "text/html",
+    "css": "text/css",
+    "js": "text/javascript",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "gif": "image/gif",
+    "webp": "image/webp",
+    "svg": "image/svg+xml",
+    "icon": "image/x-icon",
+    "webm": "video/webm",
+    "ogg": "video/ogg",
+    "mp4": "video/mp4",
+    "mp3": "audio/mpeg",
+    "ttf": "font/ttf",
+    "otf": "font/otf",
+    "woff": "font/woff",
+    "woff2": "font/woff2",
+    "pdf": "application/pdf"
 };
 
 // Hostname and Port
@@ -38,41 +33,40 @@ const port = 3000;
 
 // Create Server
 const server = http.createServer((req, res) => {
-  var uri = url.parse(req.url).pathname;
-  var fileName = path.join(process.cwd(), unescape(uri)); // Current working directory + uri
-  console.log('Loading ' + uri);
-  var stats;
+    const uri = url.parse(req.url).pathname;
+    const fileName = path.join(process.cwd(), unescape(uri)); // Current working directory + uri
+    console.log('Loading ' + uri);
+    let stats;
 
-  try {
-    stats = fs.lstatSync(fileName);
-  } catch(e) {
-    // If file not found
-    res.writeHead(404, {'Content-Type' : 'text/plain'});
-    res.write('404 not Found\n');
-    res.end();
-    return;
-  }
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        // If file not found
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
 
-  // Check if file or directory
-  if(stats.isFile()) {
-    var mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
-    res.statusCode = 200;
-    res.setHeader('Content-Type', mimeType);
-    var fileStream = fs.createReadStream(fileName);
-    fileStream.pipe(res);
-  } else if(stats.isDirectory()) {
-    res.statusCode = 302;
-    res.setHeader('Location', 'index.html');
-    res.end();
-  } else {
-    res.statusCode = 500;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('500 Internal Error\n');
-  }
-
+    // Check if file or directory
+    if (stats.isFile()) {
+        const mimeType = mimeTypes[path.extname(fileName).split('.').reverse()[0]];
+        res.statusCode = 200;
+        res.setHeader('Content-Type', mimeType);
+        const fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+        res.statusCode = 302;
+        res.setHeader('Location', 'index.html');
+        res.end();
+    } else {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('500 Internal Error\n');
+    }
 });
 
 // Run Server
 server.listen(port, hostname, () => {
-  console.log('Server running at http://' + hostname + ':' + port + '\n');
+    console.log(`Server running at http://${hostname}:${port}/\n`);
 });
